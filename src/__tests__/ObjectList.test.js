b940ba5bc38a2aaf0776c249fdf7cc13dea43952
Jarek Glowacki <jarekwg@gmail.com>
2018-03-21 17:15:59 +1100

    Add tests for selectAll

---
+++ src/__tests__/ObjectList.test.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+
+import ObjectList from '../'
+
+jest.mock('../actions-filters/ActionsFiltersContainer', () => 'ActionsFiltersContainer')
+
+describe('<ObjectList />', () => {
+  describe('Functions', () => {
+    const props = {
+      selectItems: jest.fn(),
+    }
+
+    it('handles selectAll correctly', () => {
+      spyOn(props, 'selectItems')
+      const instance = shallow(<ObjectList {...props} />).instance()
+      instance.selectAll()
+      expect(props.selectItems).toHaveBeenCalledWith('all')
+    })
+
+    it('handles deselectAll correctly', () => {
+      spyOn(props, 'selectItems')
+      const instance = shallow(<ObjectList {...props} />).instance()
+      instance.deselectAll()
+      expect(props.selectItems).toHaveBeenCalledWith(null)
+    })
+  })
+})
