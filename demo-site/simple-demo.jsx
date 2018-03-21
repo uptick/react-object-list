fd38a55d89f366ed9b4f1dbff2cb556ffd7734c1
Retief Visser <retief.visser@uptickhq.com>
2018-03-21 16:16:26 +1100

    Add react-object-list Table demos

--- demo-site/simple-demo.jsx
+++ demo-site/simple-demo.jsx
@@ -1,18 +1,33 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
-// import ObjectList from 'react-object-list'
+import ObjectList from 'react-object-list'
+
+const mockData = require('./demo.data.json').slice(0, 5)
+const columns = [
+  [
+    {dataKey: 'first_name', header: 'First Name'},
+    {dataKey: 'last_name', header: 'Last Name'},
+  ],
+  {dataKey: 'email', header: 'Email'},
+  {dataKey: 'gender', header: 'Gender'},
+]
 
 class SimpleDemo extends React.Component {
   render() {
-    return (
-      <div>Simple Demo</div>
-    );
+    return <ObjectList
+      columns={columns}
+      data={mockData}
+      meta={{
+        totalCount: mockData.length,
+      }}
+      favouritesEnabled={false}
+    />
   }
 }
 
-var mount = document.querySelectorAll('div.demo-mount-simple');
+const mount = document.querySelector('div.demo-mount-simple')
 ReactDom.render(
   <SimpleDemo />,
-  mount[0]
-);
+  mount
+)
