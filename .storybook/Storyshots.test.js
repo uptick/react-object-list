d1599be950f0ebcb560accc5c5eff768cdc0720b
Fiona Lawrence <fiona.lawrence@aba-systems.com.au>
2018-03-22 10:30:35 +1100

    Mock out info for storyshots

--- .storybook/Storyshots.test.js
+++ .storybook/Storyshots.test.js
@@ -1,5 +1,9 @@
 import initStoryshots, {multiSnapshotWithOptions} from '@storybook/addon-storyshots'
 
+jest.mock('@storybook/addon-info', () => ({
+  withInfo: () => jest.fn(story => story),
+}))
+
 initStoryshots({
   test: multiSnapshotWithOptions({})
 })
