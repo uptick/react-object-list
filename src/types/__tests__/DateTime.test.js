8da730bf22525887a0384703785cf7fd1e6bf3f1
Fiona Lawrence <fiona.lawrence@aba-systems.com.au>
2018-03-22 12:34:14 +1100

    Datetime test error

--- src/types/__tests__/DateTime.test.js
+++ src/types/__tests__/DateTime.test.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Moment from 'moment'
+import { shallow } from 'enzyme'
 import { snapshotTest } from 'utils/tests'
-import { SHORTDATE_FORMAT } from '../../utils'
+import { SHORTDATE_FORMAT, API_DATE_FORMAT } from '../../utils'
 import { DateTime } from '../'
 
-describe('Predefined cells', () => {
-  it('DateTime', () => {
+describe('DateTime', () => {
+  it('parses date correctly', () => {
     const date = Moment('2017-01-01')
     const laterDate = Moment('2017-02-01')
     const testValues = [
@@ -22,5 +23,13 @@ describe('Predefined cells', () => {
     testValues.forEach(testValue => {
       snapshotTest(<DateTime {...testValue} />)
     })
+  })
+  it('handles errors in utc', () => {
+    const dateString = '2018-02-07'
+    const instance = shallow(<DateTime dateOnly outputFormat="DD/MM/YY" />).instance()
+    spyOn(Moment, 'utc').and.callFake(() => { throw new Error('BAD') })
+    const result = instance.formatDateTime(dateString)
+    expect(Moment.utc).toHaveBeenCalledWith(dateString, API_DATE_FORMAT)
+    expect(result).toBe('07/02/18')
   })
 })
