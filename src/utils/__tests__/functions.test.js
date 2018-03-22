67285110f96a8d1dec852029cd632e96cd2db6c6
Fiona Lawrence <fiona.lawrence@aba-systems.com.au>
2018-03-22 16:28:08 +1100

    function util tests

--- src/utils/__tests__/functions.test.js
+++ src/utils/__tests__/functions.test.js
@@ -1,4 +1,4 @@
-import {getVisibleColumns} from '../functions'
+import {getVisibleColumns, sortByName} from '../functions'
 
 describe('Filters visible columns', () => {
   const notOptional = {dataKey: 'a', optional: false}
@@ -13,6 +13,11 @@ describe('Filters visible columns', () => {
     expect(getVisibleColumns(columns, [])).toEqual([[notOptional]])
     expect(getVisibleColumns(columns, ['a', 'b'])).toEqual(columns)
   })
+  it('handles empty nested array', () => {
+    const columns = [[notOptional, isOptional], []]
+    expect(getVisibleColumns(columns, [])).toEqual([[notOptional]])
+    expect(getVisibleColumns(columns, ['a', 'b'])).toEqual([columns[0]])
+  })
   it('handles mixed array', () => {
     const nestedOptional = {...isOptional, dataKey: 'c'}
     const nestedNotOptional = {...notOptional, dataKey: 'd'}
@@ -20,5 +25,32 @@ describe('Filters visible columns', () => {
     expect(getVisibleColumns(columns, [])).toEqual([notOptional, [nestedNotOptional]])
     expect(getVisibleColumns(columns, ['a', 'c'])).toEqual([notOptional, [nestedOptional, nestedNotOptional]])
     expect(getVisibleColumns(columns, ['c', 'b'])).toEqual(columns)
+  })
+})
+
+describe('Sorts by name', () => {
+  it('sorts alphabetically', () => {
+    expect(sortByName(
+      {name: 'abc'}, {name: 'def'}
+    )).toBe(-1)
+    expect(sortByName(
+      {name: 'def'}, {name: 'abc'}
+    )).toBe(1)
+  })
+  it('handles equality', () => {
+    expect(sortByName(
+      {name: 'abc'}, {name: 'abc'}
+    )).toBe(0)
+  })
+  it('ignores case', () => {
+    expect(sortByName(
+      {name: 'Abc'}, {name: 'dEF'}
+    )).toBe(-1)
+    expect(sortByName(
+      {name: 'DeF'}, {name: 'abc'}
+    )).toBe(1)
+    expect(sortByName(
+      {name: 'abc'}, {name: 'ABC'}
+    )).toBe(0)
   })
 })
