6958e8ca0f59f1dd2f60804ebc740c1304eab8ca
Fiona Lawrence <fiona.lawrence@aba-systems.com.au>
2018-05-04 12:33:03 +1000

    Tidy up tests

--- src/types/__tests__/AllSelector.test.js
+++ src/types/__tests__/AllSelector.test.js
@@ -1,27 +1,43 @@
 import React from 'react'
-import { mount } from 'enzyme'
+import { shallow, mount } from 'enzyme'
 import {
   snapshotTest,
 } from 'utils/tests'
 import { AllSelector } from '../'
 
 describe('Selector type', () => {
-  it('snapshot test', () => {
-    snapshotTest(<AllSelector />)
-    snapshotTest(<AllSelector numSelected={4} total={4} />)
+  describe('Snapshots', () => {
+    it('renders default', () => {
+      snapshotTest(<AllSelector />)
+    })
+    it('renders all selected', () => {
+      snapshotTest(<AllSelector numSelected={4} total={4} />)
+    })
   })
-  it('calls select when none selected', () => {
-    const callback = jest.fn()
-    const wrapper = mount(<AllSelector selectAll={callback} />)
-    expect(callback).not.toBeCalled()
-    wrapper.find('input').simulate('change')
-    expect(callback).toBeCalled()
+  describe('Lifecycle', () => {
+    it('updates interdeterminateness when props changed', () => {
+      spyOn(AllSelector.prototype, 'componentDidMount')
+      const wrapper = shallow(<AllSelector />)
+      const instance = wrapper.instance()
+      spyOn(instance, '__setIndeterminate')
+      wrapper.setProps({numSelected: 4, total: 10})
+      expect(instance.__setIndeterminate).toHaveBeenCalledWith(4, 10)
+    })
   })
-  it('calls deselect when some selected', () => {
-    const callback = jest.fn()
-    const wrapper = mount(<AllSelector numSelected={5} total={10} deselectAll={callback} />)
-    expect(callback).not.toBeCalled()
-    wrapper.find('input').simulate('change')
-    expect(callback).toBeCalled()
+  describe('Events', () => {
+    it('calls select when none selected', () => {
+      const callback = jest.fn()
+      const wrapper = mount(<AllSelector selectAll={callback} />)
+      expect(callback).not.toBeCalled()
+      wrapper.find('input').simulate('change')
+      expect(callback).toBeCalled()
+    })
+    it('calls deselect when some selected', () => {
+      const callback = jest.fn()
+      const wrapper = mount(<AllSelector numSelected={5} total={10} deselectAll={callback} />)
+      expect(callback).not.toBeCalled()
+      wrapper.find('input').simulate('change')
+      expect(callback).toBeCalled()
+    })
   })
 })
