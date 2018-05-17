4b6b4c34a1644c017d35e0c591181c383eed6383
Retief Visser <retief.visser@uptickhq.com>
2018-05-17 11:43:27 +1000

    Add test for boolean filter

--- src/filters/types/__tests__/Boolean.test.js
+++ src/filters/types/__tests__/Boolean.test.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import { snapshotTest } from 'utils/tests'
+import { shallow } from 'enzyme'
 import Boolean from '../Boolean'
 
 jest.mock('../Choice', () => 'Choice')
 
 describe('Boolean', () => {
+  const baseProps = {
+    onChange: jest.fn(),
+    value: 'True',
+  }
   describe('Snapshots', () => {
-    const baseProps = {
-      onChange: jest.fn(),
-      value: 'True',
-    }
     it('renders true', () => {
       snapshotTest(<Boolean {...baseProps} />)
     })
@@ -18,6 +19,18 @@ describe('Boolean', () => {
     })
     it('has custom labels', () => {
       snapshotTest(<Boolean {...baseProps} trueLabel="Very True" falseLabel="Super False" />)
+    })
+  })
+  describe('Functions', () => {
+    let instance
+    beforeEach(() => {
+      spyOn(baseProps, 'onChange')
+      instance = shallow(<Boolean {...baseProps} />).instance()
+    })
+    it('handles value change', () => {
+      const mockValue = {value: 'False', key: '1'}
+      instance.onValueChange(mockValue)
+      expect(baseProps.onChange).lastCalledWith('False')
     })
   })
 })
